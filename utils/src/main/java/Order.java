46c35695e9af728dfde4f5a5c129be597d409a41
IUTB <noreply@univ-tlse2.fr>
2021-11-09 16:05:54 +0100

    wassim eclaté aux echecs

--- utils/src/main/java/Order.java
+++ utils/src/main/java/Order.java
@@ -3,28 +3,44 @@ import java.util.LinkedList;
 
 public class Order {
 
-    private List<Drink> contents = new LinkedList<>();
+	private List<Drink> contents = new LinkedList<>();
 
-    private String owner;
-    public void setOwner(String who) { this.owner = who; }
+	private String owner;
 
-    private String recipient;
-    public void setRecipient(String who) { this.recipient = who; }
+	public void setOwner(String who) {
+		this.owner = who;
+	}
 
-    public List<Drink> getDrinks() { return contents; }    
+	private String recipient;
 
-    @Override
-    public String toString() {
-        return "Order: " + owner + " / " + recipient + " / { " + contents + "}";
-    }
+	public void setRecipient(String who) {
+		this.recipient = who;
+	}
 
-    static class Drink {
-        public Drink(String name){ this.name = name; }
-        private String name;
-        public String getName() { return name; }
-        @Override public String toString() { return name; }
-    }
-    
+	public List<Drink> getDrinks() {
+		return contents;
+	}
+
+	@Override
+	public String toString() {
+		return "Order: " + owner + " / " + recipient + " / { " + contents + "}";
+	}
+
+	static class Drink {
+		public Drink(String name) {
+			this.name = name;
+		}
+
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		@Override
+		public String toString() {
+			return name;
+		}
+	}
+
 }
-
-
